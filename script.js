bcafd38447da3bac58fc70ac199ce5715e78edc7
meetsantos <140851758+meetsantos@users.noreply.github.com>
2025-08-15 10:54:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// JS Toggle for menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+  } else {
+    mySidebar.style.display = 'block';
+  }
+}
+
+// JS Close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
